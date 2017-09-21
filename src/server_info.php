1399035e5f05cdb08b9932d17cc2b8dbacf2caba
Aung Myat Moe <aungmyatmoe2008@gmail.com>
2017-09-21 16:22:33 +0800

    enhance design

---
+++ src/server_info.php
@@ -0,0 +1,20 @@
+<!--
+/**
+ * Created by PhpStorm.
+ * User: iVideoSmart
+ * Developer: Moe
+ * Date: 21/9/2017
+ * Time: 3:44 PM
+ */
+-->
+
+<html>
+    <head>
+        <title>Server Information Details</title>
+    </head>
+    <body>
+        <?php
+            print_r($_SERVER);
+        ?>
+    </body>
+</html>
---
+++ src/shell_exec.php
@@ -0,0 +1,20 @@
+<!--
+/**
+ * Created by PhpStorm.
+ * User: iVideoSmart
+ * Developer: Moe
+ * Date: 21/9/2017
+ * Time: 3:41 PM
+ */
+-->
+
+<html>
+    <head>
+        <title>Shell Script Running Example</title>
+    </head>
+    <body>
+        <?php
+            echo shell_exec("pwd");
+        ?>
+    </body>
+</html>
